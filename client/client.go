30d48b556a8ae79b63899e50276f7eedb0baadf6
Nithin <thenithinraj@gmail.com>
2023-02-13 10:17:38 +0530

    Added Register and Deregister functionality

--- client/client.go
+++ client/client.go
@@ -1,11 +1,14 @@
 package client
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"net"
 )
 
 type Client struct {
+	Addr string
 	Conn net.Conn
 }
 
@@ -15,4 +18,20 @@ func New(loadbalanceraddr string) (*Client, error) {
 		return nil, fmt.Errorf("failed to establish connection with %v", loadbalanceraddr)
 	}
 	return &Client{Conn: conn}, nil
+}
+
+func (c *Client) Register() {
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.LittleEndian, byte(0))
+	binary.Write(buf, binary.LittleEndian, int32(len(c.Addr)))
+	binary.Write(buf, binary.LittleEndian, []byte(c.Addr))
+	c.Conn.Write(buf.Bytes())
+}
+
+func (c *Client) DeRegister() {
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.LittleEndian, byte(1))
+	binary.Write(buf, binary.LittleEndian, int32(len(c.Addr)))
+	binary.Write(buf, binary.LittleEndian, []byte(c.Addr))
+	c.Conn.Write(buf.Bytes())
 }
